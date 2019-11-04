b3e5e418529b812ea22b9ef1bd269d1e0e3e7003
nbAa <654399254@qq.com>
2019-11-04 22:22:08 +0800

    提-交

--- 02.histrory/js/index.js
+++ 02.histrory/js/index.js
@@ -13,6 +13,8 @@ $(function () {
         render($nowLi.data('page'));
         return false;
     });
+
+    
     $(window).on('popstate',function () {
         var page = 'index';
         var pathname = location.pathname;
@@ -44,57 +46,57 @@ var render = function (page) {
 /*2.渲染什么页面需要和后台提供的地址保持一致*/
 /*3.切换历史渲染的页面*/
 
-$(function () {
+$(function() {
 
-    $(".wrapper").on("click","a",function () {
+    $(".wrapper").on("click", "a", function() {
 
         //渲染页面  页面标识
-        var  page = $(this).parent().data("page");
+        var page = $(this).parent().data("page");
         render(page);
         /*地址保持一致*/
         /*追加地址 而且这个地址后台一定要存在*/
 
-        var  historyUrl = $(this).attr("href");
-        history.pushState(null,null,historyUrl);
+        var historyUrl = $(this).attr("href");
+        history.pushState(null, null, historyUrl);
 
         return false;
     })
 
     /*监听切换历史*/
-    window.onpopstate = function () {
+    window.onpopstate = function() {
 
         /*获取地址栏信息*/
         console.log(location.pathname);
         /*根据信息判断需要加载什么页面的内容*/
-        var  pathname = location.pathname;
-        var  page = "index";
-        if (pathname.indexOf("index.php")>-1){
+        var pathname = location.pathname;
+        var page = "index";
+        if (pathname.indexOf("index.php") > -1) {
             page = "index";
-        } else if (pathname.indexOf("my.php")>-1){
-            page="my"
-        } else if (pathname.indexOf("friend.php")>-1){
-            page="friend";
+        } else if (pathname.indexOf("my.php") > -1) {
+            page = "my"
+        } else if (pathname.indexOf("friend.php") > -1) {
+            page = "friend";
         }
         render(page);
     }
-    var render = function (page) {
+    var render = function(page) {
 
         /*怎么调用ajax 请求方式 请求地址 请求参数 返回数据结构和意义*/
         /*发出请求*/
         $.ajax({
-            type:"get",
-            url:"api/data.php",
-            data:{
-                page:page
+            type: "get",
+            url: "api/data.php",
+            data: {
+                page: page
             },
-            dataType:"json",
-            success:function () {
+            dataType: "json",
+            success: function() {
 
                 /*渲染页面*/
                 /*选中样式*/
                 $("[data-page]").removeClass("now");
                 /*data返回了当前的页面标识*/
-                $('[data-page="'+data.page+'"]').addClass('now');
+                $('[data-page="' + data.page + '"]').addClass('now');
 
                 /*网页内容*/
                 $(".content").html(data.html);
